224a08350646f7cf1296076b2ebe848376c5ee4e
c_cpp_a <111665869+c-cpp-a@users.noreply.github.com>
2023-06-21 18:57:29 +0800

    Update ege5star.cpp
    1. 添加了常量定义。这样子看起来更易懂？或许还可以加上注释说明，但是我模仿不了码风。
    
    2. 注释掉了fps的定义。这里面甚至没用到f。而且示例应该使用的是graphics.h/ege.h（就是要推销的ege），不应牵扯到别的。

--- demo/ege5star.cpp
+++ demo/ege5star.cpp
@@ -1,36 +1,37 @@
-// Ğı×ªÎå½ÇĞÇ¶¯»­ÑİÊ¾³ÌĞò
+// æ—‹è½¬äº”è§’æ˜ŸåŠ¨ç”»æ¼”ç¤ºç¨‹åº
 #include <graphics.h>
-#include <ege/fps.h>
+//#include <ege/fps.h>
 #include <time.h>
 #include <math.h>
 
+const double rotatingSpeed = -0.003, fullCircleRatation = PI * 2, starAngle = PI * 4 / 5;
+
 void paintstar(double x, double y, double r, double a)
 {
-    int pt[10];
-    for (int n = 0; n < 5; ++n)
-    {
-        pt[n*2] = (int)( -cos( PI * 4 / 5 * n + a ) * r + x );
-        pt[n*2+1] = (int)( sin( PI * 4 / 5 * n + a) * r + y );
-    }
-    fillpoly(5, pt);
+	int pt[10];
+	for (int n = 0; n < 5; ++n)
+	{
+		pt[n*2] = (int)( -cos( starAngle * n + a ) * r + x );
+		pt[n*2+1] = (int)( sin( starAngle * n + a) * r + y );
+	}
+	fillpoly(5, pt);
 }
 
 int main()
 {
-    initgraph( 640, 480 );
-    setrendermode(RENDER_MANUAL);
-    double r = 0;
-    fps f;
-    for ( ; is_run(); delay_fps(1000) )
-    {
-        r += 0.02;
-        if (r > PI * 2) r -= PI * 2;
-
-        cleardevice();
-        setcolor( EGERGB(0xff, 0xff, 0xff) );
-        setfillcolor( EGERGB(0, 0, 0xff) );
-        paintstar(300, 200, 100, r);
-    }
-    return 0;
+	initgraph( 640, 480 );
+	setrendermode(RENDER_MANUAL);
+	double r = 0;
+//	fps f;
+	for ( ; is_run(); delay_fps(1000) )
+	{
+		r += rotatingSpeed;
+		if (r > fullCircleRatation) r -= fullCircleRatation;
+		
+		cleardevice();
+		setcolor( EGERGB(0xff, 0xff, 0xff) );
+		setfillcolor( EGERGB(0, 0, 0xff) );
+		paintstar(300, 200, 100, r);
+	}
+	return 0;
 }
-
